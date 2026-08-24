83fb6e6da8242f643c08bfd0fe5ba6103238a94e
revanrevan12 <alidowandi1@gmail.com>
2026-08-25 05:50:58 +0800

    chore: android generator (demo) v1.0.20

--- .github/scripts/gen-android.js
+++ .github/scripts/gen-android.js
@@ -565,6 +565,7 @@ function installLauncherIcon() {
       'import android.annotation.SuppressLint;',
       'import android.app.NotificationChannel;',
       'import android.app.NotificationManager;',
+      'import android.app.PendingIntent;',
       'import android.content.Context;',
       'import android.content.Intent;',
       'import android.net.Uri;',
